67148f32e6b11591c2e57d77227d54a9ddbd4453
Ryan de Ridder <rjd3rjd@gmail.com>
2014-03-09 17:23:19 -0400

    Through RORT 7.6

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,6 +19,11 @@ describe "User Pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "after submission" do
+        before {click_button submit}
+        it { should have_selector('title', text: 'Sign Up', visible: false) }
+        it { should have_content('error') }
+      end
     end
     describe "with valid information" do
       before do
@@ -29,6 +34,12 @@ describe "User Pages" do
       end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe "after saving the user" do
+        before {click_button submit}
+        let(:user) { User.find_by_email('user@example.com') }
+        it { should have_selector('title', text: user.name, visible: false) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
