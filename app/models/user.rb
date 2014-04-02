40943f3aa2eaf744844743829b842db6570967ff
Ryan de Ridder <rjd3rjd@gmail.com>
2014-04-01 21:50:00 -0400

    Add user following. Through RoRT 11.4. Through Sample App Creation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@
 
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   before_save { |user| user.email = email.downcase } # before_save { self.email.downcase! } # also would work
   before_create :create_remember_token
@@ -24,7 +28,19 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    self.relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    self.relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
   def User.new_remember_token
